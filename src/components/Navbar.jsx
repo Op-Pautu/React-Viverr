01566acb2671fa007395fd09275f9224d5927766
Op Pautu <pautuop@gmail.com>
2023-03-06 12:59:39 +0530

    Initial files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.scss";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <span className="text">viverr</span>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Viverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign In</span>
+
+          <span>Become a seller</span>
+
+          <button>Join</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
