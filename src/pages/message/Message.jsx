b1639ef5a4fcb7c12a5cfa4c30739e472b2c60b8
Op Pautu <pautuop@gmail.com>
2023-03-08 01:29:11 +0530

    Add basic structure for message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,7 +1,39 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./Message.scss";
 function Message() {
-  return <div>Message</div>;
+  return (
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">MESSAGES</Link> &gt; Jane Doe &gt;
+        </span>
+        <div className="messages">
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+        </div>
+        <div className="write">
+          <textarea
+            name=""
+            id=""
+            placeholder="Write a message"
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Message;
